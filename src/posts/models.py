6ec8e157d44f80785d3275746c5eb02169a36943
mohamedahmed60 <mohamedaljabri6666@gmail.com>
2022-12-18 23:39:36 +0300

    add module and user

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+'''
+    1 : fields
+    2 : validation
+    3 : html widget
+
+    لتنفيذ قاعدة البيانات هناك شيئين نقوم بعملها 
+    1 : makemigrations
+    2 : migrate
+'''
+
+class Post(models.Model):
+    name = models.FloatField(max_length=100) # str max lenght
+    content = models.TextField(max_length=5000)
